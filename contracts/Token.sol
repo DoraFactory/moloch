8345ed3035a9f562785a6b85bc232b88ede8674f
bugarnet <bluegarnet10@gmail.com>
2021-07-22 03:12:29 +0400

    update the lao

--- contracts/Token.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity >0.6.0;
-
-import "./oz/ERC20.sol";
-
-contract Token is ERC20 {
-    constructor(uint256 supply) public ERC20("Moloch USD", "MUSD", 18) {
-        _mint(msg.sender, supply);
-    }
-}
